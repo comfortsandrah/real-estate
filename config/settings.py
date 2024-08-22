c75974bd5944220183ad73bf6473c2d3d9604fff
dancoon <dancoondev@gmail.com>
2024-08-22 10:05:21 +0300

    Update settings

--- config/settings.py
+++ config/settings.py
@@ -178,13 +178,13 @@ SOCIALACCOUNT_PROVIDERS = {
         }
     },
 }
-EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_BACKEND="django.core.mail.backends.console.EmailBackend"
+# EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND="django.core.mail.backends.console.EmailBackend"
 
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "dancunian10340@gmail.com"
-EMAIL_HOST_PASSWORD = "yxua gbqx xkmv xeyn"
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD = ""
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = True
-DEFAULT_FROM_EMAIL = "dancunian10340@gmail.com"
+DEFAULT_FROM_EMAIL = ""
