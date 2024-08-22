291dcbffe55b2877c10cb41dfad44801568e3d98
dancoon <dancoondev@gmail.com>
2024-08-22 08:33:03 +0300

    Add contact us

--- core/views.py
+++ core/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from . import forms
+from django.contrib import messages
 
 
 # Create your views here.
@@ -9,7 +10,13 @@ def home(request):
 
 def contact(request):
     context = {"title": "Contact Us", "form": forms.ContactForm()}
+    if request.method == "POST":
+        form = forms.ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+        messages.success(request, "Your message has been sent successfully!")
     return render(request, "contact.html", context=context)
+
 
 def about(request):
     return render(request, "about.html")
