174e4c5026d77352f1255a0cc852440a77706646
Sandrah <comfortsandrah13@gmail.com>
2024-08-22 07:30:05 +0300

    Create links and styling

--- core/urls.py
+++ core/urls.py
@@ -4,4 +4,6 @@ from . import views
 app_name = "core"
 urlpatterns = [
     path("", views.home, name="home"),
+    path("about/", views.about, name="about" ),
+    path("contact/", views.contact, name="contact" ),
 ]
